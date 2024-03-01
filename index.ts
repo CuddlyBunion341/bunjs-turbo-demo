804964478ea27409d5872fa6773cc3ad149b49ce
Daniel Bengl <daniel.bengl@renuo.ch>
2024-03-01 09:08:45 +0100

    Implement basic bun server

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log("Hello via Bun!");
+Bun.serve({
+  port: 8080,
+  fetch(req) {
+    const url = new URL(req.url);
+    if (url.pathname === "/") return new Response("Home page!");
+    if (url.pathname === "/blog") return new Response("Blog!");
+    return new Response("404!");
+  },
+});
+
+console.log("Bun is running on http://localhost:8080")
+
+
+console.log("Bun is running on http://localhost:8080")
