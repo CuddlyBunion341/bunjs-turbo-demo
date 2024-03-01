af6bdd9d903e57ea072ef40fe89de2c8782fb319
Daniel Bengl <daniel.bengl@renuo.ch>
2024-03-01 09:09:32 +0100

    Implement content type HTML

--- index.ts
+++ index.ts
@@ -1,9 +1,23 @@
+const headers = {
+  "Content-Type": "text/html",
+}
+
 Bun.serve({
   port: 8080,
   fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname === "/") return new Response("Home page!");
-    if (url.pathname === "/blog") return new Response("Blog!");
+    if (url.pathname === "/") return new Response(`
+      <h1>Homepage</h1>
+      <p>This is a homepage</p>
+    `, {headers});
+    if (url.pathname === "/article1") return new Response(`
+      <h1>Article1</h1>
+      <p>This is the first article page</p>
+    `, {headers});
+    if (url.pathname === "/article2") return new Response(`
+      <h1>Article2</h1>
+      <p>This is the second article page</p>
+    `, {headers});
     return new Response("404!");
   },
 });
