c2a768b155cd70a3375616feb4a82a6eceead023
Daniel Bengl <daniel.bengl@renuo.ch>
2024-03-01 11:03:24 +0100

    Implement chat room (broken)

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
 console.log("Client is running!")
+
+const client = new WebSocket("ws://localhost:8080")
+const form = document.getElementById("chat-form")
+const chatFeed = document.getElementById("chat-feed")
+
+client.onmessage = (message) => {
+  chatFeed.innerHTML += `<div>${message.data}</div>`
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  const formData = new FormData(form)
+  const message = formData.get("message")
+  client.send(message)
+  form.reset()
+})
--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ const layout = (title: string, content: string) => `
     <head>
       <title>${title}</title>
       <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
-      <script src="./client.js"></script>
+      <script src="./client.js" defer></script>
     </head>
     <body>
       <div class="container">
@@ -22,13 +22,30 @@ Bun.serve({
   port: 8080,
   fetch(req) {
     const url = new URL(req.url);
-    console.log(url.pathname)
-    if (url.pathname === "/") return new Response(layout("Homepage", "<p>This is a homepage</p>"), {headers});
-    if (url.pathname === "/article1") return new Response(layout("Article1", "<p>This is the first article page</p>"), {headers});
-    if (url.pathname === "/article2") return new Response(layout("Article2", "<p>This is the second article page</p>"), {headers});
+    if (url.pathname === "/") return new Response(layout("Chatroom", `
+    <p>This is a chatroom</p>
+    <div id="chat-feed"></div>
+    <form id="chat-form">
+      <label for="message-input">Message</label>
+      <input id="message-input">
+      <input type="submit" value="Send">
+    </form>`), {headers});
     if (url.pathname === "/client.js") return new Response(Bun.file("./client.js"), {headers: {"Content-Type": "text/javascript"}});
     return new Response("404!");
   },
+  websocket: {
+    open(ws) {
+      console.log("Websocket opened")
+      ws.subscribe("my-topic")
+    },
+    message(ws, message) {
+      console.log("Websocket received message: ", message)
+      ws.send("Hello from server");
+    },
+    close(ws) {
+      console.log("Websocket closed")
+    }
+  }
 });
 
 console.log("Bun is running on http://localhost:8080")
