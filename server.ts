0f423c45e2aabd3fd04949be35e43ba982c8a02e
Daniel Bengl <daniel.bengl@renuo.ch>
2024-03-01 10:49:39 +0100

    Create client

--- server.ts
+++ server.ts
@@ -7,6 +7,7 @@ const layout = (title: string, content: string) => `
     <head>
       <title>${title}</title>
       <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+      <script src="./client.js"></script>
     </head>
     <body>
       <div class="container">
@@ -21,9 +22,11 @@ Bun.serve({
   port: 8080,
   fetch(req) {
     const url = new URL(req.url);
+    console.log(url.pathname)
     if (url.pathname === "/") return new Response(layout("Homepage", "<p>This is a homepage</p>"), {headers});
     if (url.pathname === "/article1") return new Response(layout("Article1", "<p>This is the first article page</p>"), {headers});
     if (url.pathname === "/article2") return new Response(layout("Article2", "<p>This is the second article page</p>"), {headers});
+    if (url.pathname === "/client.js") return new Response(Bun.file("./client.js"), {headers: {"Content-Type": "text/javascript"}});
     return new Response("404!");
   },
 });
