0e3812741cd3ec2dca327e58639d1a9c5098c8a5
Daniel Bengl <daniel.bengl@renuo.ch>
2024-03-01 09:09:53 +0100

    Implement layout

--- index.ts
+++ index.ts
@@ -2,27 +2,30 @@ const headers = {
   "Content-Type": "text/html",
 }
 
+const layout = (title: string, content: string) => `
+  <html>
+    <head>
+      <title>${title}</title>
+      <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+    </head>
+    <body>
+      <div class="container">
+        <h1>${title}</h1>
+        ${content}
+      </div>
+    </body>
+  </html>
+`;
+
 Bun.serve({
   port: 8080,
   fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname === "/") return new Response(`
-      <h1>Homepage</h1>
-      <p>This is a homepage</p>
-    `, {headers});
-    if (url.pathname === "/article1") return new Response(`
-      <h1>Article1</h1>
-      <p>This is the first article page</p>
-    `, {headers});
-    if (url.pathname === "/article2") return new Response(`
-      <h1>Article2</h1>
-      <p>This is the second article page</p>
-    `, {headers});
+    if (url.pathname === "/") return new Response(layout("Homepage", "<p>This is a homepage</p>"), {headers});
+    if (url.pathname === "/article1") return new Response(layout("Article1", "<p>This is the first article page</p>"), {headers});
+    if (url.pathname === "/article2") return new Response(layout("Article2", "<p>This is the second article page</p>"), {headers});
     return new Response("404!");
   },
 });
-
-console.log("Bun is running on http://localhost:8080")
-
 
 console.log("Bun is running on http://localhost:8080")
